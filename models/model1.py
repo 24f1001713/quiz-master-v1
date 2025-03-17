58108f2a12fed9a4272441c80f1699283079d369
24f1001713 <24f1001713@ds.study.iitm.ac.in>
2025-03-18 00:13:08 +0530

    Added database schema for Quiz, Question and Scores

--- models/model1.py
+++ models/model1.py
@@ -1,19 +1,46 @@
 from controllers.database import db
 
 class User(db.Model):
-    id = db.Column(db.Integer(),primary_key=True)
-    username=db.Column(db.String(),unique=True,nullable=False)
-    email=db.Column(db.String(),unique=True,nullable=False)
-    password=db.Column(db.String(),nullable=False)
-    type=db.Column(db.String(),default="general")
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(),unique=True,nullable=False)
+    email = db.Column(db.String(),unique=True,nullable=False)
+    password = db.Column(db.String(),nullable=False)
+    score_details = db.relationship("Scores",backref="user_details")
 
 class Subject(db.Model):
-    id = db.Column(db.Integer(),primary_key=True)
-    subject = db.Column(db.String(),unique=True,nullable=False)
-    sub_id = db.Column(db.String(),unique=True,nullable=False)
+    id = db.Column(db.Integer,primary_key=True)
+    subject_name = db.Column(db.String(),unique=True,nullable=False)
+    sub_description = db.Column(db.String(),nullable=False)
     chap_details = db.relationship("Chapter",backref="sub_details")
 
 class Chapter(db.Model):
-    id = db.Column(db.Integer(),primary_key=True)
-    chapter = db.Column(db.String(),unique=True,nullable=False)
-    subject = db.Column(db.String(),db.ForeignKey("subject.subject"),nullable=False)
+    id = db.Column(db.Integer,primary_key=True)
+    chapter_name = db.Column(db.String(),unique=True,nullable=False)
+    subject_id = db.Column(db.Integer,db.ForeignKey("subject.id"),nullable=False)
+    quiz_details = db.relationship("Quiz",backref="chapter_details")
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    quiz_name = db.Column(db.String(),unique=True,nullable=False)
+    chapter_id = db.Column(db.Integer,db.ForeignKey("chapter.id"),nullable=False)
+    deadline = db.Column(db.String(),nullable=False)
+    duration = db.Column(db.String(),nullable=False)
+    question_details = db.relationship("Question",backref="quiz_details")
+    score_details = db.relationship("Scores",backref="quiz_details")
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    qno = db.Column(db.Integer, nullable=False)
+    quiz_id = db.Column(db.Integer,db.ForeignKey("quiz.id"),nullable=False)
+    question_statement = db.Column(db.Text, nullable=False)
+    option1 = db.Column(db.String(), nullable=False)
+    option2 = db.Column(db.String(), nullable=False)
+    option3 = db.Column(db.String(), nullable=False)
+    option4 = db.Column(db.String(), nullable=False)
+    correct_option = db.Column(db.Integer, nullable=False)
+
+class Scores(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    quiz_id = db.Column(db.Integer,db.ForeignKey("quiz.id"),nullable=False)
